5dda0826b733bedec34b3555d98f34c067a806d6
gungdikaebs <gungdika88@gmail.com>
2023-02-23 22:11:24 +0800

    Membuat Pembayaran

--- view/history.php
+++ view/history.php
@@ -12,7 +12,7 @@ require "../koneksi.php"; ?>
     <link rel="stylesheet" href="../css/tableview.css">
     <link rel="stylesheet" href="../css/navbar.css">
 
-    <title>Document</title>
+    <title>Aplikasi Pembayaran SPP | History Pembayaran</title>
 </head>
 
 <body>
@@ -21,6 +21,55 @@ require "../koneksi.php"; ?>
     <div class="container">
         <div class="card-tittle">
             <h4>History Pembayaran</h4>
+        </div>
+
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nis</th>
+                        <th>Tanggal Bayar</th>
+                        <th>Bulan</th>
+                        <th>Tahun</th>
+                        <th>Jumlah</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if ($_SESSION['leveluser'] == 'admin' || $_SESSION['leveluser'] == 'petugas') {
+                        $query = mysqli_query($koneksi, "SELECT * FROM tb_pembayaran");
+                        while ($data = mysqli_fetch_assoc($query)) {
+                    ?>
+                            <tr>
+                                <td><?php echo $data['id_pembayaran']; ?></td>
+                                <td><?php echo $data['nis']; ?></td>
+                                <td><?php echo $data['tgl_bayar']; ?></td>
+                                <td><?php echo $data['bulan']; ?></td>
+                                <td><?php echo $data['tahun']; ?></td>
+                                <td><?php echo $data['jumlah']; ?></td>
+                            </tr>
+                        <?php
+                        }
+                    } else if ($_SESSION['leveluser'] == 'siswa') {
+                        $nis = $_SESSION['nis'];
+                        $query = mysqli_query($koneksi, "SELECT * FROM tb_pembayaran WHERE nis='$nis'");
+                        while ($data = mysqli_fetch_assoc($query)) {
+                        ?>
+                            <tr>
+                                <td><?php echo $data['id_pembayaran']; ?></td>
+                                <td><?php echo $data['nis']; ?></td>
+                                <td><?php echo $data['tgl_bayar']; ?></td>
+                                <td><?php echo $data['bulan']; ?></td>
+                                <td><?php echo $data['tahun']; ?></td>
+                                <td><?php echo $data['jumlah']; ?></td>
+                            </tr>
+                    <?php
+                        }
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
 
