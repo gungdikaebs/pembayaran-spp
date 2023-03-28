dc5c7888986ddfa314a6db58d3fbc2aab3fa26a2
gungdikaebs <gungdika88@gmail.com>
2023-03-28 16:38:39 +0800

    Complete

--- view/dashboard.php
+++ view/dashboard.php
@@ -15,16 +15,20 @@ if (!isset($_SESSION['username'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/tableview.css">
     <title>Dashboard | Aplikasi Pembayaran SPP</title>
 </head>
 
 <body>
     <?php require "../template/navbar.php";
     $nis = $_SESSION['username'];
-
     ?>
 
     <div class="container">
+        <div class="card-tittle">
+            <h4>Welcome Back, <span><?= $_SESSION['username']; ?></span></h4>
+        </div>
+
         <div class="row">
 
         </div>
